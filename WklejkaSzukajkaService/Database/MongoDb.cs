15bad3380347b70c8c5f066e5d7cb33545733b9f
ogendogen <kosmos500@interia.pl>
2021-01-10 19:17:14 +0100

    Created mongodb class

---
+++ WklejkaSzukajkaService/Database/MongoDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Database.Models;
+using MongoDB.Driver;
+
+namespace Database
+{
+    public class MongoDb
+    {
+        public MongoClient MongoClient { get; set; }
+        public IMongoDatabase MongoDatabase { get; set; }
+        public IMongoCollection<DocEntry> DocsCollection { get; set; }
+        
+        public MongoDb(string connectionString)
+        {
+            MongoClient = new MongoClient(connectionString);
+            
+            MongoDatabase = MongoClient.GetDatabase("wklejto");
+            DocsCollection = MongoDatabase.GetCollection<DocEntry>("docs");
+        }
+    }
+}

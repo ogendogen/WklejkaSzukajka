3ce71e25076c58f70f8ad1e73607e41c66317606
ogendogen <kosmos500@interia.pl>
2021-01-10 19:16:54 +0100

    Created DocEntry model

---
+++ WklejkaSzukajkaService/Database/Models/DocEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Database.Models
+{
+    public class DocEntry
+    {
+        public int PageID { get; set; }
+        [BsonIgnoreIfNull]
+        public string Author { get; set; }
+        [BsonIgnoreIfDefault]
+        public DateTime Date { get; set; }
+        [BsonIgnoreIfDefault]
+        public string Content { get; set; }
+        [BsonIgnoreIfDefault]
+        public string PicturePath { get; set; }
+        [BsonIgnoreIfNull]
+        public byte[] Picture { get; set; }
+        [BsonIgnoreIfDefault]
+        public bool IsPasswordProtected { get; set; }
+        [BsonIgnoreIfDefault]
+        public bool IsDeleted { get; set; }
+    }
+}

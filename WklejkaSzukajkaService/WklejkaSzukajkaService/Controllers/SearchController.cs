89c8dc0141a0dcf19c5285e415e83e8ebf89d2ba
ogendogen <kosmos500@interia.pl>
2021-01-10 20:06:27 +0100

    SearchController with GetByContains method

---
+++ WklejkaSzukajkaService/WklejkaSzukajkaService/Controllers/SearchController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database;
+using Database.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WklejkaSzukajkaService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly ILogger<SearchController> _logger;
+        private readonly IDatabase _database;
+
+        public SearchController(ILogger<SearchController> logger, IDatabase database)
+        {
+            _logger = logger;
+            _database = database;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<DocEntry>> GetByContains(string condition)
+        {
+            return await _database.GetDocsByContains(condition);
+        }
+    }
+}
